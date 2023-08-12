c98fefd49b89a1d44b20fb9316ccdce6f50dad85
avkav <95727249+avkav@users.noreply.github.com>
2023-08-12 09:45:33 +0200

    Update WebConfig.java
    .allowedMethods("*") was added

--- src/main/java/com/ejemplo/SpringBoot/WebConfig.java
+++ src/main/java/com/ejemplo/SpringBoot/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer{
     
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("*"); //.allowedMethods("*") was added
     }
 }
